16abd0cc8939218d8babd16da9689c77c437bdb5
zhangxietao <zhangxt7@chinatelecom.cn>
2024-06-21 11:01:26 +0800

    fix: 引入ef组件

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div style="margin: 20px;">
-    <ef-table
-      rowKey="id"
-      :rows="tableData" :cols="columns"
-      @expand-change="onExpand"
-    ></ef-table>
+    <ef-table :rows="tableData" :cols="columns" @filter-change="onFilterChange">
+      <template #tag="{ row }">
+        <el-tag :type="row.tag === '家' ? 'primary' : 'success'">{{ row.tag }}</el-tag>
+      </template>
+    </ef-table>
   </div>
 </template>
 
@@ -18,27 +18,51 @@
     data() {
       return {
         columns: [
-          { type: 'expand', cellClassName: row => {
-              if (row._index === 0) {
-                return false;
-              }
-              if (row._index === 1) {
-                return 1;
-              }
-              if (row._index === 2) {
-                return 'expand-custom';
-              }
-            } },
-          { prop: 'date', label: '日期' },
+          { prop: 'date', label: '日期', sortable: true },
           { prop: 'name', label: '姓名' },
-          { prop: 'address', label: '地址' }
+          { prop: 'address', label: '地址' },
+          {
+            prop: 'tag',
+            columnKey: 'tag',
+            label: '标签',
+            filters: [
+              { text: '家', value: '家' },
+              { text: '公司', value: '公司' }
+            ],
+            filterMethod: (value, row) => row.tag === value
+          }
         ],
-        tableData: getTestData()
+        tableData: [
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄',
+            tag: '家'
+          },
+          {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄',
+            tag: '公司'
+          },
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄',
+            tag: '家'
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+            tag: '公司'
+          }
+        ]
       }
     },
     methods: {
-      onExpand(row, expanded) {
-        console.log("=>(index.vue:31) expanded", expanded);
+      onFilterChange(val) {
+        console.log("=>(index.vue:65) val", val);
       }
     }
   };
